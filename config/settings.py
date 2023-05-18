382cb7ddd058c72ee483382048c011bf4a42c395
Ravshan <ravshangiyosov2002@gmail.com>
2023-05-18 21:53:17 +0500

    asda

--- config/settings.py
+++ config/settings.py
@@ -30,10 +30,7 @@ ADMIN = ENV.get('GROUP_ID')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(ENV.get('DEBUG') == '1')
-# CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "https://tourguide.travel",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = ['backend.tourguide.travel']
 SITE_URL = ENV.get('SITE_URL')
 
